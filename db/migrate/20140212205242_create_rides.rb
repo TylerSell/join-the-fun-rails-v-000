0efbeb2900bf7b907c08b5b6c5e89bffb82b1608
Tyler Sell <tylerselldesign@gmail.com>
2020-04-06 17:07:47 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
-    create_table :rides do |t|
+    create_join_table :taxis, :passengers, table_name: :rides do |t|
+      t.index :taxi_id
+      t.index :passenger_id
       t.timestamps null: false
     end
   end

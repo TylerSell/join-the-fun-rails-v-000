d52fdc1244e7ffc68a6a3d7a65c4d4de137d6f87
Tyler Sell <tylerselldesign@gmail.com>
2020-04-06 17:35:43 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200406172932) do
+ActiveRecord::Schema.define(version: 20200406173433) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,13 +18,9 @@ ActiveRecord::Schema.define(version: 20200406172932) do
     t.integer  "ride_id"
   end
 
-  create_table "rides", id: false, force: :cascade do |t|
-    t.integer  "taxi_id",      null: false
-    t.integer  "passenger_id", null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["passenger_id"], name: "index_rides_on_passenger_id"
-    t.index ["taxi_id"], name: "index_rides_on_taxi_id"
+  create_table "rides", force: :cascade do |t|
+    t.integer "taxi_id"
+    t.integer "passenger_id"
   end
 
   create_table "taxis", force: :cascade do |t|

b8e08f361e6aca2aa2deb593918636711a2cbde1
Tyler Sell <tylerselldesign@gmail.com>
2020-04-06 17:30:56 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200406171749) do
+ActiveRecord::Schema.define(version: 20200406172932) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
